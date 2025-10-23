780bc28b1993e23f39644b675b19edfd2d8e4f5f
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-10-23 15:22:58 +0530

    add Toaster component for enhanced user notifications in App

--- client-editor/src/App.tsx
+++ client-editor/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import Home from './pages/HomePage.tsx'
 import './App.css'
 
@@ -6,12 +7,40 @@ function App() {
 
 
   return (
+    <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              iconTheme: {
+                // Changed this from the old green to your new accent color
+                primary: '#4DD0E1',
+                secondary: '#FFFFFF',
+              },
+              // You might also want to set the text color to white for contrast
+              style: {
+                background: '#4DD0E1',
+                color: '#FFFFFF',
+              }
+            },
+            // You can also style error toasts, etc.
+            error: {
+              iconTheme: {
+                primary: '#FF5A5F', // A common red for errors
+                secondary: '#FFFFFF',
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
