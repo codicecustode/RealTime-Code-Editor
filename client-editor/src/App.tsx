5400ce928f4474e9b08261a42e5b6ba9ab7f4baf
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-10-23 12:22:29 +0530

    add App component structure and styles for HomePage

--- client-editor/src/App.tsx
+++ client-editor/src/App.tsx
@@ -1,11 +1,17 @@
- import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/HomePage.tsx'
+import './App.css'
 
 function App() {
-  
+
 
   return (
-         
-    
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
