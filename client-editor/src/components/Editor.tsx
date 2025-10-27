547b2761fefac06a5b177dfa515d44be4353588a
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-10-27 23:40:06 +0530

    implement CodeMirror editor with JavaScript support and VS Code theme

--- client-editor/src/components/Editor.tsx
+++ client-editor/src/components/Editor.tsx
@@ -1,5 +1,37 @@
-const Editor = () => {
-    return <div>Editor Component</div>;
-};
+import { useRef, useEffect } from "react";
 
+import { basicSetup } from "codemirror"
+import { EditorView } from "@codemirror/view"
+// import {
+//     defaultHighlightStyle, syntaxHighlighting
+// } from "@codemirror/language"
+import { javascript } from "@codemirror/lang-javascript"; // Any language support you need
+import { vscodeDark } from "@uiw/codemirror-theme-vscode"; // VS Code dark theme
+
+
+
+const Editor = () => {
+    const editorRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const editor = new EditorView({
+            doc: "// Happy Coding!",
+            extensions: [
+                basicSetup,
+                javascript(),      // Add languages as required
+                vscodeDark,        // The VS Code theme extension
+            ],
+            parent: editorRef.current!,
+        });
+
+        return () => {
+            editor.destroy();
+        }
+    }, []);
+
+
+    return (
+        <div ref={editorRef} className="editor"></div>
+    )
+}
 export default Editor;
