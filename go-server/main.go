0ece88792a65dc29d6042e5e134e9326c66bb848
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-10-28 23:10:00 +0530

    refactor websocket handling to use structured message types and manage rooms

--- go-server/main.go
+++ go-server/main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-	
 
 	"github.com/gorilla/websocket"
-	
 )
 
 type Message struct {
 	Event string `json: "event"`
 	Data string `json: "data"`
+	RoomId string `json: "roomId"`
 }
 
-//mu := sync.Mutex
-var clients = make(map[*websocket.Conn] bool)
-var messageChan = make(chan []byte)
+// mu := sync.Mutex
+var messageChan = make(chan Message)
+var rooms = make(map[string]map[*websocket.Conn]bool)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -27,17 +26,29 @@ var upgrader = websocket.Upgrader{
 }
 
 func handlerConnection(w http.ResponseWriter, r *http.Request) {
+	roomId := r.URL.Query().Get("roomId")
+	if roomId == "" {
+		log.Fatal("Room Id is required to connect with Server.")
+		return
+	}
+
+	if _, exist := rooms[roomId]; !exist {
+		rooms[roomId] = make(map[*websocket.Conn]bool)
+	}
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Upgrade",err)
 		return
 	}
 
 	//always close the connection(Although this line not close instant, it schedule the closing connection)
+	
 	defer conn.Close()
 
-	clients[conn] = true
-	messageChan <- []byte("A user joined")
+
+	rooms[roomId][conn] = true
+
+	messageChan <- Message { Event: "join", Data: "A user Joined", RoomId: roomId}
 
 	for {
 		//send message to the client
@@ -46,7 +57,6 @@ func handlerConnection(w http.ResponseWriter, r *http.Request) {
 		// 	log.Fatal(err)
 		// 	return
 		// }
-	
 
 		// //read the message from client
 		// _, message, err := conn.ReadMessage()
@@ -54,40 +64,55 @@ func handlerConnection(w http.ResponseWriter, r *http.Request) {
 		// 	log.Fatal(err)
 		// 	return
 		// }
-		
+
 		var message Message
 		err := conn.ReadJSON(&message)
 		if err != nil {
 			log.Fatal(err)
 		}
 		switch message.Event {
-			case "message":
-				messageChan <- []byte (message.Data)
-			case "join":
-				messageChan <- []byte ("A user Joined room")
+		case "message":
+			messageChan <- Message{Event: "message", Data: "Some Message Received", RoomId: roomId}
+		case "join":
+			messageChan <- Message{Event: "join", Data: "A user Joined", RoomId: roomId}
+		case "codeChange":
+
 		}
 	}
 }
 
-func broadcastMessage(){
+func broadcastMessage() {
 
 	for {
-	// receive the broadcasting message from message channel
-		msg := <- messageChan 
+		// receive the broadcasting message from message channel
+		// msg := <-messageChan
 
-		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, msg)
-			if err!= nil{
-				log.Fatal("Erro while writing broadcasting msg.", err)
-				delete(clients, client)
+		// for client := range clients {
+		// 	err := client.WriteMessage(websocket.TextMessage, msg)
+		// 	if err != nil {
+		// 		log.Fatal("Erro while writing broadcasting msg.", err)
+		// 		delete(clients, client)
+		// 	}
+		// }
+
+		msg := <-messageChan
+		roomId = msg.roomId
+		for client := range rooms[roomId] {
+			err := client.WriteMessage(websocket.TextMessage,msg.data)
+			if err != nil {
+				log.Fatal(err)
+				delete(rooms[roomId], client)
 			}
 		}
-  }
+	}
 }
 
 func main() {
 	http.HandleFunc("/ws", handlerConnection)
+
+	//use goroutine for concurrency
 	go broadcastMessage()
+
 	log.Println("Server started on 8080 port")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
